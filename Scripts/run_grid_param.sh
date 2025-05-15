8fdfe8cc239fcc2cc243f26b6034c1f6686eba27
Trazz <trazz.pepper@umconnect.umt.edu>
2025-05-15 20:20:37 +0000

    ready for cluster

--- Scripts/run_grid_param.sh
+++ Scripts/run_grid_param.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=Trazz_Timecorr_Param_Sweep
-#SBATCH --output=/mnt/beegfs/hellgate/home/tp183485/timecorr_trazzp/slurm-out/param-sweep.%a.out
-#SBATCH --error=/mnt/beegfs/hellgate/home/tp183485/timecorr_trazzp/slurm-out/param-sweep.%a.err
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=1
 #SBATCH --array=0-15
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tp183485@umconnect.umt.edu
 
-# you can cd anywhere now, logs will always go to that exact folder
-mkdir -p /mnt/beegfs/hellgate/home/tp183485/timecorr_trazzp/slurm-out
+LOGDIR=/mnt/beegfs/hellgate/home/tp183485/timecorr_trazzp/slurm-out
+mkdir -p "$LOGDIR"
+
 cd /mnt/beegfs/hellgate/home/tp183485/timecorr_trazzp
 source timecorr_venv/bin/activate
-
 cd Scripts
 
-# 2) pick the weight‐function name based on the array ID
-#    (make sure the order here matches the 10 you want)
 PARAMS=(
     PCA
     IncrementalPCA
@@ -38,7 +34,10 @@ PARAMS=(
 )
 WP=${PARAMS[$SLURM_ARRAY_TASK_ID]}
 
-# 3) run your script
-python Cluster_Grid_Param_TRAZZ.py intact 100 10 10 isfc "$WP" 5 gaussian
+OUT="$LOGDIR/param-sweep.${WP}.out"
+ERR="$LOGDIR/param-sweep.${WP}.err"
 
-echo "[$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID] exit code: $?"
+python Cluster_Grid_Param_TRAZZ.py intact 100 10 10 isfc "$WP" 5 gaussian \
+    >"$OUT" 2>"$ERR"
+
+echo "[$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID] ($WP) exit code: $?" >> "$OUT"
