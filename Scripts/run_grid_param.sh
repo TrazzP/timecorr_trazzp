022af3abe0be317a11d9112de97d58c8028006aa
Trazz <trazz.pepper@umconnect.umt.edu>
2025-05-15 20:31:53 +0000

    final changes to script

--- Scripts/run_grid_param.sh
+++ Scripts/run_grid_param.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=Trazz_Timecorr_Param_Sweep
-#SBATCH --time=12:00:00
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+#SBATCH --time=10:00:00
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-15
+#SBATCH --array=0-16
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tp183485@umconnect.umt.edu
 
@@ -14,30 +16,19 @@ cd /mnt/beegfs/hellgate/home/tp183485/timecorr_trazzp
 source timecorr_venv/bin/activate
 cd Scripts
 
-PARAMS=(
-    PCA
-    IncrementalPCA
-    SparsePCA
-    MiniBatchSparsePCA
-    KernelPCA
-    FastICA
-    FactorAnalysis
-    TruncatedSVD
-    DictionaryLearning
-    MiniBatchDictionaryLearning
-    TSNE
-    Isomap
-    SpectralEmbedding
-    LocallyLinearEmbedding
-    MDS
-    UMAP
-)
+PARAMS=( PCA IncrementalPCA SparsePCA MiniBatchSparsePCA KernelPCA FastICA \
+         FactorAnalysis TruncatedSVD DictionaryLearning MiniBatchDictionaryLearning \
+         TSNE Isomap SpectralEmbedding LocallyLinearEmbedding MDS UMAP )
+
 WP=${PARAMS[$SLURM_ARRAY_TASK_ID]}
 
 OUT="$LOGDIR/param-sweep.${WP}.out"
 ERR="$LOGDIR/param-sweep.${WP}.err"
 
-python Cluster_Grid_Param_TRAZZ.py intact 100 10 10 isfc "$WP" 5 gaussian \
-    >"$OUT" 2>"$ERR"
+# Redirect everything from here on
+exec >"$OUT" 2>"$ERR"
 
-echo "[$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID] ($WP) exit code: $?" >> "$OUT"
+
+python Cluster_Grid_Param_TRAZZ.py intact 100 10 10 isfc "$WP" 5 gaussian
+
+echo "[$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID] ($WP) exit code: $?" 
